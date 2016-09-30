7ab284f45dbc86d30b1b4aab1bfd63a793247e8e
Jason Arnold <jason.arnold.00@gmail.com>
2016-09-29 22:28:25 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,44 @@
 # your code goes here
+require 'pry'
+class Person
+
+  attr_accessor :bank_account
+  attr_reader :name, :happiness, :hygiene
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def happiness=(num)
+    @happiness = [[10, num].min, 0].max
+  end
+
+  def hygiene=(num)
+    @hygiene = [[10, num].min, 0].max
+  end
+
+  def happy?
+    @happiness > 7
+  end
+
+  def clean?
+    @hygiene > 7
+  end
+
+  def get_paid(salary)
+    @bank_account += salary
+    "all about the benjamins"
+  end
+
+  def take_bath
+    @hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+end
+
+
+
